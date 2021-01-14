6783f64c5de536d8818f38243b06b799f290ad21
Mikolaj <mikolaj.mikolaj@gmail.com>
2021-01-14 21:30:38 +0100

    fixed error where data_remover would only remove every second entry

--- PythonScraper/data_remover.py
+++ PythonScraper/data_remover.py
@@ -5,9 +5,9 @@ def removeRecordsNotContainingKeywords(pcParts):
 
     for entry in pcPartsCopy:
         for product in entry["products"]:
-            for item in product["items"]:
-                if not containsKeywords(item["name"], product["mustInclude"]):
-                    product["items"].remove(item)
+            product["items"] = [item for item in product["items"] if
+            containsKeywords(item["name"], product["mustInclude"])]
+            
     return pcPartsCopy
 
 def containsKeywords(pcPartName, keywords):
