31326914b7b3f42c21dc47ba0cf459c8cf616a7a
Mikolaj <mikolaj.mikolaj@gmail.com>
2021-02-14 14:11:50 +0100

    add database connection

---
+++ WCF_and_API/PcPartsScrap/PcPartsScrap.Wcf.Data/PcPartsDbContext.cs
@@ -0,0 +1,21 @@
+﻿using PcPartsScrap.Wcf.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PcPartsScrap.Wcf.Data
+{
+	class PcPartsDbContext : DbContext
+	{
+		public PcPartsDbContext() : base("name=PCPartsDB")
+		{
+
+		}
+
+		public DbSet<PcPart> PcParts { get; set; }
+		public DbSet<ExchangeRate> ExchangeRates { get; set; }
+	}
+}
